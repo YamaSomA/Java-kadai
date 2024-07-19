9e160f548d8347a8ec2a854c49a49d0a3315e4d9
YamaSomA <poiuytrgh321@gmail.com>
2024-07-19 10:16:23 +0900

    ファイルを追加

---
+++ kitasoftkoubou_kadai/Javakadai/src/kadai/JavaKadai_104.java
@@ -0,0 +1,16 @@
+package kadai;
+
+public class JavaKadai_104 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+        int x;
+        
+        x = 13 + 17;
+
+        System.out.println( "x=" + x );
+
+	}
+
+}
---
+++ kitasoftkoubou_kadai/Javakadai/src/kadai/JavaKadai_112.java
@@ -0,0 +1,20 @@
+package kadai;
+
+public class JavaKadai_112 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+        int x = 10;
+
+        x++;
+        
+        System.out.println( x );
+        
+        x--;
+        x--;
+        
+        System.out.println( x );
+	}
+
+}
