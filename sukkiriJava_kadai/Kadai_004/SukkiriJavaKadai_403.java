a0afc4f207cf2220c7500897dda90c5abe8cc9a6
YamaSomA <poiuytrgh321@gmail.com>
2024-07-26 13:27:03 +0900

    ファイルを追加

---
+++ sukkiriJava_kadai/Kadai_004/SukkiriJavaKadai_403.java
@@ -0,0 +1,14 @@
+package sukkiriJavaKadai;
+
+public class SukkiriJavaKadai_403 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	
+		05 NullPointerException
+		06 ArrayIndexOutOfBoundsException
+		
+		
+	}
+
+}
