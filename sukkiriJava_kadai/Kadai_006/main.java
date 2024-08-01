1d2ad58bd0826b5ddb8eda2beab19c71783d7745
YamaSomA <poiuytrgh321@gmail.com>
2024-08-01 10:05:40 +0900

    ファイルを追加

---
+++ sukkiriJava_kadai/Kadai_006/main.java
@@ -0,0 +1,17 @@
+package main;
+
+import comment.Zenhan;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	    Zenhan.doWarusa(); 
+	    Zenhan.doTogame(); 
+	    comment.Kouhan.callDeae(); 
+	    comment.Kouhan.showMondokoro(); 
+	    
+	}
+
+}
