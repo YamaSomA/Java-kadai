a20fb4f9ad54dc6dcbfad55ebc9b7334b395e7f3
YamaSomA <poiuytrgh321@gmail.com>
2024-09-13 11:22:59 +0900

    ファイルを追加

---
+++ kitasoftkoubou_kadai/Kadai_004/kitasoftkoubou_401/Exercise.java
@@ -0,0 +1,10 @@
+package kitasoftkoubou_401;
+
+import java.io.IOException;
+
+public class Exercise {
+	public static void main(String[] args) throws IOException {
+		for (int i = 0; i < 10; i++)
+			System.out.print("SPAM");
+	}
+}
---
+++ kitasoftkoubou_kadai/Kadai_004/kitasoftkoubou_402/Exercise.java
@@ -0,0 +1,10 @@
+package kitasoftkoubou_402;
+
+import java.io.IOException;
+
+public class Exercise {
+	public static void main(String[] args) throws IOException {
+		for (int i = 1; i <= 9; i++)
+			System.out.print((3 * i) + " ");
+	}
+}
